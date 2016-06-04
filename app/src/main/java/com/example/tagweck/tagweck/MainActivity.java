9fe82599d499fba289ce000032195bbccd023a01
PhilippRudolph <philipprudolph1990@gmail.com>
2016-06-04 10:27:07 +0200

    0.1

--- app/src/main/java/com/example/tagweck/tagweck/MainActivity.java
+++ app/src/main/java/com/example/tagweck/tagweck/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.tagweck.tagweck;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import java.lang.*;
@@ -10,42 +12,94 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView seconds;
-    private TextView minutes;
-    private TextView hours;
+    public static TextView seconds_text;
+    public static TextView minutes_text;
+    public static TextView hours_text;
+
+    public static int seconds;
+    public static int minutes;
+    public static int hours;
+
+    static final String seconds_string = "Sekunden";
+    static final String minutes_string = "Minuten";
+    static final String hours_string = "Stunden";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        seconds = (TextView)findViewById(R.id.seconds);
-        minutes = (TextView)findViewById(R.id.minutes);
-        hours = (TextView)findViewById(R.id.hours);
+        seconds_text = (TextView)findViewById(R.id.seconds_text);
+        minutes_text = (TextView)findViewById(R.id.minutes_text);
+        hours_text = (TextView)findViewById(R.id.hours_text);
 
-        seconds.setText(Integer.toString(getseconds()));
-        minutes.setText(Integer.toString(getminutes()));
-        hours.setText(Integer.toString(gethours()));
+        if (savedInstanceState != null) {
+            seconds = savedInstanceState.getInt(seconds_string);
+            minutes = savedInstanceState.getInt(minutes_string);
+            hours = savedInstanceState.getInt(hours_string);
+            seconds_text.setText(Integer.toString(seconds));
+            minutes_text.setText(Integer.toString(minutes));
+            hours_text.setText(Integer.toString(hours));
+        }
+        else {
+            SharedPreferences prefs = getPreferences(Context.MODE_PRIVATE);
+
+        //    if (prefs.contains("initialisiert")) {
+                seconds_text.setText(Integer.toString(prefs.getInt(seconds_string, getseconds())));
+                minutes_text.setText(Integer.toString(prefs.getInt(minutes_string, getminutes())));
+                hours_text.setText(Integer.toString(prefs.getInt(hours_string, gethours())));
+        //    }
+        //    else {
+        //        seconds_text.setText(Integer.toString(getseconds()));
+        //        minutes_text.setText(Integer.toString(getminutes()));
+        //        hours_text.setText(Integer.toString(gethours()));
+        //    }
+        }
+
+        //seconds.setText(Integer.toString(getseconds()));
+        //minutes.setText(Integer.toString(getminutes()));
+        //hours.setText(Integer.toString(gethours()));
+    }
+
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt(seconds_string, seconds);
+        savedInstanceState.putInt(minutes_string, minutes);
+        savedInstanceState.putInt(hours_string, hours);
+    }
+
+    public void onPause() {
+        super.onPause();
+
+        SharedPreferences prefs = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(seconds_string, seconds);
+        editor.putInt(minutes_string, minutes);
+        editor.putInt(hours_string, hours);
+        editor.putBoolean("initialisiert", true);
+        editor.commit();
     }
 
     private int getseconds() {
         Calendar c = Calendar.getInstance();
-        int seconds;
-        return seconds = c.get(Calendar.SECOND);
+        int sec = c.get(Calendar.SECOND);
+        seconds = sec;
+        return sec;
     }
     private int getminutes() {
         Calendar c = Calendar.getInstance();
-        int minutes;
-        return minutes = c.get(Calendar.MINUTE);
+        int min = c.get(Calendar.MINUTE);
+        minutes = min;
+        return min;
     }
     private int gethours() {
         Calendar c = Calendar.getInstance();
-        int hours;
-        return hours = c.get(Calendar.HOUR_OF_DAY);
+        int h = c.get(Calendar.HOUR_OF_DAY);
+        hours = h;
+        return h;
     }
     public void refresh(View view) {
-        seconds.setText(Integer.toString(getseconds()));
-        minutes.setText(Integer.toString(getminutes()));
-        hours.setText(Integer.toString(gethours()));
+        seconds_text.setText(Integer.toString(getseconds()));
+        minutes_text.setText(Integer.toString(getminutes()));
+        hours_text.setText(Integer.toString(gethours()));
     }
 }
