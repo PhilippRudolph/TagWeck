98bcd96d51d9fe284927ef4bd2033470cf6cdbe9
PhilippRudolph <philipprudolph1990@gmail.com>
2016-11-13 14:07:57 +0100

    0.1

--- app/src/main/java/com/example/tagweck/tagweck/MainActivity.java
+++ app/src/main/java/com/example/tagweck/tagweck/MainActivity.java
@@ -6,15 +6,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import java.lang.*;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 
+import android.widget.Button;
 import android.widget.TextView;
 import android.view.View;
+import android.widget.TimePicker;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static TimePicker timePicker;
+    public static ToggleButton alarm_toggle;
+    public static Button button_ok;
 
     public static TextView seconds_text;
     public static TextView minutes_text;
     public static TextView hours_text;
+    public static TextView weckzeit;
 
     public static int seconds;
     public static int minutes;
@@ -23,6 +32,13 @@ public class MainActivity extends AppCompatActivity {
     static final String seconds_string = "Sekunden";
     static final String minutes_string = "Minuten";
     static final String hours_string = "Stunden";
+    static final String hour_string = "Stunde";
+    static final String minute_string = "Minute";
+
+    public static int hour;
+    public static int minute;
+
+    Calendar cWeckzeit = new GregorianCalendar();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,39 +48,24 @@ public class MainActivity extends AppCompatActivity {
         seconds_text = (TextView)findViewById(R.id.seconds_text);
         minutes_text = (TextView)findViewById(R.id.minutes_text);
         hours_text = (TextView)findViewById(R.id.hours_text);
+        weckzeit = (TextView)findViewById(R.id.weckzeit);
+        timePicker = (TimePicker)findViewById(R.id.timePicker);
+        alarm_toggle = (ToggleButton)findViewById(R.id.alarm_toogle);
+        button_ok = (Button)findViewById(R.id.button_ok);
 
-        if (savedInstanceState != null) {
-            seconds = savedInstanceState.getInt(seconds_string);
-            minutes = savedInstanceState.getInt(minutes_string);
-            hours = savedInstanceState.getInt(hours_string);
-            seconds_text.setText(Integer.toString(seconds));
-            minutes_text.setText(Integer.toString(minutes));
-            hours_text.setText(Integer.toString(hours));
-        }
-        else {
-            SharedPreferences prefs = getPreferences(Context.MODE_PRIVATE);
+        timePicker.setIs24HourView(true);
+        timePicker.setCurrentHour(Calendar.getInstance().get(Calendar.HOUR_OF_DAY));
 
-        //    if (prefs.contains("initialisiert")) {
-                seconds_text.setText(Integer.toString(prefs.getInt(seconds_string, getseconds())));
-                minutes_text.setText(Integer.toString(prefs.getInt(minutes_string, getminutes())));
-                hours_text.setText(Integer.toString(prefs.getInt(hours_string, gethours())));
-        //    }
-        //    else {
-        //        seconds_text.setText(Integer.toString(getseconds()));
-        //        minutes_text.setText(Integer.toString(getminutes()));
-        //        hours_text.setText(Integer.toString(gethours()));
-        //    }
-        }
+        SharedPreferences prefs = getPreferences(Context.MODE_PRIVATE);
 
-        //seconds.setText(Integer.toString(getseconds()));
-        //minutes.setText(Integer.toString(getminutes()));
-        //hours.setText(Integer.toString(gethours()));
-    }
-
-    public void onSaveInstanceState(Bundle savedInstanceState) {
-        savedInstanceState.putInt(seconds_string, seconds);
-        savedInstanceState.putInt(minutes_string, minutes);
-        savedInstanceState.putInt(hours_string, hours);
+        seconds_text.setText(Integer.toString(prefs.getInt(seconds_string, getseconds())));
+        minutes_text.setText(Integer.toString(prefs.getInt(minutes_string, getminutes())));
+        hours_text.setText(Integer.toString(prefs.getInt(hours_string, gethours())));
+        hour = prefs.getInt(hour_string, 0);
+        minute = prefs.getInt(minute_string, 0);
+        cWeckzeit.set(Calendar.HOUR_OF_DAY, hour);
+        cWeckzeit.set(Calendar.MINUTE, minute);
+        weckzeit.setText(cWeckzeit.get(Calendar.HOUR_OF_DAY) + ":" + cWeckzeit.get(Calendar.MINUTE));
     }
 
     public void onPause() {
@@ -75,6 +76,8 @@ public class MainActivity extends AppCompatActivity {
         editor.putInt(seconds_string, seconds);
         editor.putInt(minutes_string, minutes);
         editor.putInt(hours_string, hours);
+        editor.putInt(hour_string, hour);
+        editor.putInt(minute_string, minute);
         editor.putBoolean("initialisiert", true);
         editor.commit();
     }
@@ -85,21 +88,32 @@ public class MainActivity extends AppCompatActivity {
         seconds = sec;
         return sec;
     }
+
     private int getminutes() {
         Calendar c = Calendar.getInstance();
         int min = c.get(Calendar.MINUTE);
         minutes = min;
         return min;
     }
+
     private int gethours() {
         Calendar c = Calendar.getInstance();
         int h = c.get(Calendar.HOUR_OF_DAY);
         hours = h;
         return h;
     }
+
     public void refresh(View view) {
         seconds_text.setText(Integer.toString(getseconds()));
         minutes_text.setText(Integer.toString(getminutes()));
         hours_text.setText(Integer.toString(gethours()));
+    }
+
+    public void weckzeit_setzen(View view) {
+        hour = timePicker.getCurrentHour();
+        minute = timePicker.getCurrentMinute();
+        cWeckzeit.set(Calendar.HOUR_OF_DAY, hour);
+        cWeckzeit.set(Calendar.MINUTE, minute);
+        weckzeit.setText(cWeckzeit.get(Calendar.HOUR_OF_DAY) + ":" + cWeckzeit.get(Calendar.MINUTE));
     }
 }
